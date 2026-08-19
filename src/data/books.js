2fd80f5f14e99bff1fc868b0ed75809b6648cb5c
Dyovana <dyo.menegatti@hotmail.com>
2026-08-19 17:04:42 -0300

    feat: adiciona dados para seção livros lidos

---
+++ src/data/books.js
@@ -0,0 +1,137 @@
+const booksCards = [
+    {
+        id: 1,
+        image: 'https://m.media-amazon.com/images/I/61y5iLUKS3L._AC_UF1000,1000_QL80_.jpg',
+        title: 'Melhor do que nos Filmes',
+        author: 'Lynn Painter',
+        date: 'Fevereiro/2025'
+    },
+    {
+        id: 2,
+        image: 'https://m.media-amazon.com/images/I/813BWVj4ATL._SL1500_.jpg',
+        title: 'Asas Reluzentes',
+        author: 'Alisson Saft',
+        date: 'Março/2025'
+    },
+    {
+        id: 3,
+        image: 'https://m.media-amazon.com/images/I/619M4wfLBSL._AC_UF1000,1000_QL80_.jpg',
+        title: 'Noites Brancas',
+        author: 'Fiódor Dostoiévski',
+        date: 'Abril/2025'
+    },
+    {
+        id: 4,
+        image: 'https://m.media-amazon.com/images/I/81ZWaq8qigL._SL1500_.jpg',
+        title: 'A doceria mágica da rua do anoitecer',
+        author: 'Hiyoko Kurisu',
+        date: 'Maio/2025'
+    },
+    {
+        id: 5,
+        image: 'https://m.media-amazon.com/images/I/71pw2IDO+7L._AC_UF1000,1000_QL80_.jpg',
+        title: 'Vou te receitar um gato',
+        author: 'Ishida Syou',
+        date: 'Maio/2025'
+    },
+    {
+        id: 6,
+        image: 'https://m.media-amazon.com/images/I/91mdEwOzdGS._SL1500_.jpg',
+        title: 'Uma vida pequena',
+        author: 'Hanya Yanagihara',
+        date: 'Junho/2025'
+    },
+    {
+        id: 7,
+        image: 'https://darkside.vtexassets.com/arquivos/ids/167609/111-o-colecionador.jpg?v=636802523172600000',
+        title: 'O Colecionador',
+        author: 'Jon Fowles',
+        date: 'Agosto/2025'
+    },
+    {
+        id: 8,
+        image: 'https://m.media-amazon.com/images/I/81DG3c24VUL._SL1500_.jpg',
+        title: 'O Concorrente',
+        author: 'Stephen King',
+        date: 'Setembro/2025'
+    },
+    {
+        id: 9,
+        image: 'https://m.media-amazon.com/images/I/81FBrdjKQ2L._SL1500_.jpg',
+        title: 'A Zona Morta',
+        author: 'Stephen King',
+        date: 'Outubro/2025'
+    },
+    {
+        id: 10,
+        image: 'https://m.media-amazon.com/images/I/91EGWa-kYRL._SL1500_.jpg',
+        title: 'A pequena loja de venenos',
+        author: 'Sarah Penner',
+        date: 'Novembro/2025'
+    },
+    {
+        id: 11,
+        image: 'https://m.media-amazon.com/images/I/81DCIt1vHpL._SL1500_.jpg',
+        title: 'Casamento em Dezembro',
+        author: 'Sarah Morgan',
+        date: 'Dezembro/2025'
+    },
+    {
+        id: 12,
+        image: 'https://m.media-amazon.com/images/I/81HHMaBsCaL._SL1500_.jpg',
+        title: 'O bosque das coisas perdidas',
+        author: 'Shea Ernshaw',
+        date: 'Janeiro/2026'
+    },
+    {
+        id: 13,
+        image: 'https://m.media-amazon.com/images/I/71+xnMayx-L._UF1000,1000_QL80_.jpg',
+        title: 'O clube do crime das quintas-feiras',
+        author: 'Richard Osman',
+        date: 'Fevereiro/2026'
+    },
+    {
+        id: 14,
+        image: 'https://m.media-amazon.com/images/I/71Zrfk0JYFL._SL1500_.jpg',
+        title: 'Sorte no amor',
+        author: 'Lynn Painter',
+        date: 'Março/2026'
+    },
+    {
+        id: 15,
+        image: 'https://m.media-amazon.com/images/I/81NGvQ0QxdL._SL1500_.jpg',
+        title: 'Battle Royale',
+        author: 'Koushun Takami',
+        date: 'Abril/2026'
+    },
+    {
+        id: 16,
+        image: 'https://m.media-amazon.com/images/I/81UUGTUvPKL._SL1500_.jpg',
+        title: 'Uma janela sombria',
+        author: 'Rachel Gilling',
+        date: 'Maio/2026'
+    },
+    {
+        id: 17,
+        image: 'https://m.media-amazon.com/images/I/91I5pEHHpNL._SL1500_.jpg',
+        title: 'Duas coroas retorcidas',
+        author: 'Rachel Gilling',
+        date: 'Junho/2026'
+    },
+    {
+        id: 18,
+        image: 'https://m.media-amazon.com/images/I/71cVUaGAvJL._SL1500_.jpg',
+        title: 'A primeira mentira',
+        author: 'Ashley Elston',
+        date: 'Julho/2026'
+    },
+    {
+        id: 19,
+        image: 'https://m.media-amazon.com/images/I/91rcxMhZZiL._SL1500_.jpg',
+        title: 'O Casal Perfeito',
+        author: 'Ruth Ware',
+        date: 'Agosto/2026'
+    },
+];
+
+export default booksCards;
